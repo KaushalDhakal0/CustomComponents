31769837b9c48e1ec2df1ce54db2abad0e0690c6
KaushalDhakal <kaushaldhakal90@gmail.com>
2022-01-01 22:19:07 -0800

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
-import Accordion from "./Components/Accordin/Question";
+// import Accordion from "./Components/Accordin/Question";
+import Main from "./Components/Menu/Main";
 function App() {
-  return <Accordion />;
+  return (
+    <>
+      {/* <Accordion /> */}
+      <Main />
+    </>
+  );
 }
 
 export default App;
